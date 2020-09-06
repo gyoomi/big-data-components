8868c9581868b811fa0727e41d1128bde3810e4a
leon <gyoomi0709@foxmail.com>
2020-09-06 19:41:13 +0800

    feat(mapreduce): rjoin驱动编写

--- hadoop/mapreduce-of-hadoop/src/main/java/com/gyoomi/hadoop/mapreduce/rjoin/RJoin.java
+++ hadoop/mapreduce-of-hadoop/src/main/java/com/gyoomi/hadoop/mapreduce/rjoin/RJoin.java
@@ -7,12 +7,17 @@
 package com.gyoomi.hadoop.mapreduce.rjoin;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -111,7 +116,24 @@ public class RJoin
 
 	public static void main(String[] args) throws Exception
 	{
-		// TODO
+		Configuration configuration = new Configuration();
+		Job job = Job.getInstance(configuration);
+		job.setJarByClass(RJoin.class);
+
+		job.setMapperClass(RjoinMapper.class);
+		job.setReducerClass(RJoinReducer.class);
+
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputValueClass(InfoBean.class);
+
+		job.setOutputKeyClass(InfoBean.class);
+		job.setOutputValueClass(NullWritable.class);
+
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		boolean res = job.waitForCompletion(true);
+		System.exit(res ? 0 :1);
 	}
 
 }
