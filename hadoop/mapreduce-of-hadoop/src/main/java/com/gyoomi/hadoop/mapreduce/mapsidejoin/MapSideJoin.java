2a2b9f9a4b3c04f4e17a65474d83624761677302
gyoomi <gyoomi0709@foxmail.com>
2020-09-09 14:34:28 +0800

    fix(mapreduce): map side join的使用

--- hadoop/mapreduce-of-hadoop/src/main/java/com/gyoomi/hadoop/mapreduce/mapsidejoin/MapSideJoin.java
+++ hadoop/mapreduce-of-hadoop/src/main/java/com/gyoomi/hadoop/mapreduce/mapsidejoin/MapSideJoin.java
@@ -6,12 +6,22 @@
 
 package com.gyoomi.hadoop.mapreduce.mapsidejoin;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +39,19 @@ public class MapSideJoin
 		private Map<String, String> productCache = new HashMap<>();
 
 		@Override
+		protected void setup(Context context) throws IOException, InterruptedException
+		{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("pdts.txt")));
+			String productLineValue;
+			while (StringUtils.isNotBlank(productLineValue = reader.readLine()))
+			{
+				String[] fields = productLineValue.split(",");
+				productCache.put(fields[0], fields[1]);
+			}
+			reader.close();
+		}
+
+		@Override
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
 		{
 			String lineText = value.toString();
@@ -38,6 +61,32 @@ public class MapSideJoin
 			context.write(orderKey, NullWritable.get());
 		}
 
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Configuration configuration = new Configuration();
+
+		Job job = Job.getInstance(configuration);
+		job.setJarByClass(MapSideJoin.class);
+		job.setMapperClass(MapSideJoinMapper.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+
+		FileInputFormat.setInputPaths(job, new Path("D:/bigdata/mapjoininput"));
+		FileOutputFormat.setOutputPath(job, new Path("D:/bigdata/mapjoinoutput1"));
+		// 指定需要缓存一个文件到所有的maptask运行节点工作目录
+		/* job.addArchiveToClassPath(archive); */// 缓存jar包到task运行节点的classpath中
+		/* job.addFileToClassPath(file); */// 缓存普通文件到task运行节点的classpath中
+		/* job.addCacheArchive(uri); */// 缓存压缩包文件到task运行节点的工作目录
+		/* job.addCacheFile(uri) */// 缓存普通文件到task运行节点的工作目录
+
+		job.addCacheFile(new URI("file:/D:/bigdata/pdts.txt"));
+
+		// map端join的逻辑不需要reduce阶段，设置reducetask数量为0
+		job.setNumReduceTasks(0);
+		boolean res = job.waitForCompletion(true);
+		System.exit(res ? 0 : 1);
 	}
 
 }
